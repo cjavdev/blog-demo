ca4c5a88511d430f076eff711b3adec8f5b27476
CJ Avilla <cjavilla@gmail.com>
2021-09-06 10:46:58 -0700

    Add turbo streams to post and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id
   has_many :likes, as: :likeable
+
+  after_create_commit { broadcast_prepend_to(post, :comments) }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes, as: :likeable
+
+  after_update_commit { broadcast_update }
 end
